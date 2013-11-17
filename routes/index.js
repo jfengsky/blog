c6121a0d955637e04d0152e04b01122473f7a654
jfeng <jfengsky@gmail.com>
2013-11-17 10:37:06 +0800

    modify blog

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+
+/*
+ * GET home page.
+ */
+var readdirp = require('readdirp');
+
+module.exports = function(app){
+
+  readdirp({ 
+    root: './views', 
+    directoryFilter: ['!*_mods'], 
+    fileFilter: [ '*.jade' ]
+  }).on('data', function (entry) {
+
+    // console.log(entry.path);
+    var url = entry.path.slice(0, -5);
+    console.log(url);
+    
+    app.get('/' + url + '.html', function(req, res){
+      res.render(url, { title: 'index'});
+    });
+
+  });
+
+
+};
